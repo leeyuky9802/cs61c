932e8b5b4ad35c5fbc0cd744df847cad1eb1d663
Xiao <leeyuky9802@gmail.com>
2022-08-30 03:28:05 -0500

    finally done... leant nothing

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *seen[100];
+    int point = 0;
+
+    seen[point] = head;
+    head = head->next;
+
+    while(head!=NULL){
+        for(int i=0; i<=point;i++){
+            if(head == seen[i])
+                return 1;
+        }
+        point ++;
+        seen[point] = head;
+        head = head->next;
+
+    }
     return 0;
 }
